d97037249430308987172c5ed1c9e72cad11d7af
суперкомп <@DEMIN-ALEX>
2023-03-18 17:45:38 +0700

    Добавьте файлы проекта.

---
+++ lab 5/City.cs
@@ -0,0 +1,9 @@
+﻿namespace lab_5
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public GeoLocation Location { get; set; }
+    }
+
+}
---
+++ lab 5/GeoLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace lab_5
+{
+    public class GeoLocation
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+}
